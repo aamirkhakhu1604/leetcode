ff205a4db9b697c8f4309cf943f7f14f6aaa089c
aamirkhakhu1604 <138868400+aamirkhakhu1604@users.noreply.github.com>
2026-01-31 15:34:22 +0530

    Update MaxConsecutiveOnes2.java

--- problems/MaxConsecutiveOnes2.java
+++ problems/MaxConsecutiveOnes2.java
@@ -1,1 +1,28 @@
 
+class MaxConsecutiveOnes2 {
+  
+  public int Solution(int[] nums) {
+    int left = 0;
+    int right = 0;
+    int max = 0;
+    int numZeros = -1;
+
+    while (right < nums.length) {
+      if (nums[right] == 0) {
+        numZeros++;
+      }
+
+      while (numZeros >= 2) {
+        if (nums[left] == 0) {
+          numZeros--;
+        } 
+        left++;
+      }
+
+      max = Math.max(max, right - left + 1);
+      right++;
+    }
+
+    return max;
+  }
+}
