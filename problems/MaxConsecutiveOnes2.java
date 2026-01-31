9564c75b095b87525676922afd53c83de74af136
aamirkhakhu1604 <138868400+aamirkhakhu1604@users.noreply.github.com>
2026-01-31 15:40:33 +0530

    Update MaxConsecutiveOnes2.java

--- problems/MaxConsecutiveOnes2.java
+++ problems/MaxConsecutiveOnes2.java
@@ -1,28 +1,38 @@
+package org.example;
 
+/*
+Given a binary array, find the maximum number of consecutive 1s in this array if you can flip at most one 0.
+*/
 class MaxConsecutiveOnes2 {
-  
-  public int Solution(int[] nums) {
-    int left = 0;
-    int right = 0;
-    int max = 0;
-    int numZeros = -1;
 
-    while (right < nums.length) {
-      if (nums[right] == 0) {
-        numZeros++;
-      }
+    public static int solution(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int max = 0;
+        int numZeros = 0;
 
-      while (numZeros >= 2) {
-        if (nums[left] == 0) {
-          numZeros--;
-        } 
-        left++;
-      }
+        while (right < nums.length) {
+            if (nums[right] == 0) {
+                numZeros++;
+            }
 
-      max = Math.max(max, right - left + 1);
-      right++;
+            while (numZeros >= 2) {
+                if (nums[left] == 0) {
+                    numZeros--;
+                }
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
+            right++;
+        }
+
+        return max;
     }
 
-    return max;
-  }
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 0, 1, 1, 0})); // should return 4
+        System.out.println(solution(new int[]{1, 0, 1, 1, 0, 1})); // should return 4
+        System.out.println(solution(new int[]{1, 0, 1, 1, 0, 1, 1, 0, 1})); // should return 5
+    }
 }
